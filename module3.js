4fa94c49b5694c5c2830608fce9926a50fd12bf5
PolinaKrasovskaya <72392978+PolinaKrasovskaya@users.noreply.github.com>
2021-05-09 23:06:59 +0300

    Update module3.js

--- module3.js
+++ module3.js
@@ -743,3 +743,45 @@ const atTheOldToad = {
 
 41
 
+const atTheOldToad = {
+  potions: [
+    { name: 'Зелье скорости', price: 460 },
+    { name: 'Дыхание дракона', price: 780 },
+    { name: 'Каменная кожа', price: 520 },
+  ],
+  // Пиши код ниже этой строки
+  getPotions() {
+    return this.potions;
+  },
+  
+  addPotion(newPotion) {
+    if (this.potions.includes(newPotion)) {
+      return `Зелье ${newPotion} уже есть в инвентаре!`;
+    }
+
+    this.potions.push(newPotion);
+  },
+  
+  removePotion(potionName) {
+    for (const potion of this.potions) {
+      if (potion.name === potionName) {
+    const potionIndex = this.potions.indexOf(potion);
+
+    if (potionIndex === -1) {
+      return `Зелья ${potionName} нет в инвентаре!`;
+    }
+
+    this.potions.splice(potionIndex, 1);
+      }
+    }
+  },
+  
+  updatePotionName(oldName, newName) {
+    for (const potion of this.potions) {
+      if (potion.name === oldName) {
+    potion.name = newName;
+      }
+    }
+  },
+  // Пиши код выше этой строки
+};
