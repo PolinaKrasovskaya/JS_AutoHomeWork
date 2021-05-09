c477b5f95418f20c33346a4afd053471206b18d7
PolinaKrasovskaya <72392978+PolinaKrasovskaya@users.noreply.github.com>
2021-05-09 22:30:57 +0300

    Create module3.js

--- module3.js
+++ module3.js
@@ -240,3 +240,506 @@ for (const key in apartment) {
   // Пиши код выше этой строки
 }
 
+
+// 12
+// Напиши функцию countProps(object), которая считает и возвращает количество собственных 
+// свойств объекта в параметре object. Используй переменную propCount для хранения количества свойств объекта.
+
+function countProps(object) {
+  let propCount = 0;
+  // Пиши код ниже этой строки
+  for (const key in object) {
+if (object.hasOwnProperty(key)) {
+  propCount += 1;
+ }
+  }
+  // Пиши код выше этой строки
+  return propCount;
+}
+
+
+// 13
+// Перебери объект apartment используя метод Object.keys() и цикл for...of. 
+// Запиши в переменную keys массив ключей собственных свойств объекта apartment, и добавь 
+// в массив values все значения его свойств.
+
+const apartment = {
+  descr: 'Просторная квартира в центре',
+  rating: 4,
+  price: 2153,
+};
+const values = [];
+// Пиши код ниже этой строки
+const keys = Object.keys(apartment);
+for (const key of keys) {
+  values.push(apartment[key]);
+}
+
+
+// 14
+// Выполни рефакторинг функции countProps(object) используя метод Object.keys() и, возможно, цикл for...of.
+
+
+function countProps(object) {
+  // Пиши код ниже этой строки
+  let propCount = 0;
+const keys = Object.keys(object);
+for (const key of keys) {
+  propCount += 1;
+}
+
+
+  return propCount;
+  // Пиши код выше этой строки
+}
+
+
+
+
+
+// 15
+// Запиши в переменную keys массив ключей собственных свойств объекта apartment, 
+//     а в переменную values массив всех значений его свойств. Используй методы Object.keys() и Object.values().
+    
+const apartment = {
+  descr: 'Просторная квартира в центре',
+  rating: 4,
+  price: 2153,
+};
+// Пиши код ниже этой строки
+const keys = Object.keys(apartment);
+const values = Object.values(apartment);
+
+
+// 16
+// Напиши функцию countTotalSalary(salaries) которая принимает объект зарплат, где имя свойства это имя сотрудника, а значение свойства это зарплата. 
+// Функция должна рассчитать общую сумму зарплат сотрудников и вернуть её. Используй переменную totalSalary для хранения общей суммы зарплаты.
+
+function countTotalSalary(salaries) {
+  let totalSalary = 0;
+  // Пиши код ниже этой строки
+const values = Object.values(salaries);
+  for (const value of values) {
+    totalSalary += value;
+  }
+  // Пиши код выше этой строки
+  return totalSalary;
+}
+
+// 17
+// Перебери массив объектов colors используя цикл for...of. 
+// Добавь в массив hexColors значения свойств hex, а в массив rgbColors значения свойств rgb из всех объектов массива colors.
+const colors = [
+  { hex: '#f44336', rgb: '244,67,54' },
+  { hex: '#2196f3', rgb: '33,150,243' },
+  { hex: '#4caf50', rgb: '76,175,80' },
+  { hex: '#ffeb3b', rgb: '255,235,59' },
+];
+
+const hexColors = [];
+const rgbColors = [];
+// Пиши код ниже этой строки
+for (const color of colors) {
+  hexColors.push(color.hex);
+  rgbColors.push(color.rgb);
+}
+
+// 18
+// Напиши функцию getProductPrice(productName) которая принимает один параметр productName - название продукта. 
+// Функция ищет объект продукта с таким именем (свойство name) в массиве products и возвращает его цену (свойство price). 
+// Если продукт с таким названием не найден, функция должна возвращать null.
+
+const products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Сканер', price: 2700, quantity: 3 },
+  { name: 'Дроид', price: 400, quantity: 7 },
+  { name: 'Захват', price: 1200, quantity: 9 },
+];
+
+function getProductPrice(productName) {
+  // Пиши код ниже этой строки
+  for (const product of products) {
+    if (product['name'] === productName) {
+      return product['price'];
+    }
+  }
+    return null;
+  
+  // Пиши код выше этой строки
+}
+
+
+// 19
+// Напиши функцию getAllPropValues(propName) которая принимает один параметр propName - имя (ключ) свойства. 
+// Функция должна вернуть массив всех значений свойства с таким именем из каждого объекта в массиве products. 
+// Если в объектах нет свойства с таким именем, функция должна вернуть пустой массив.
+
+
+const products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Сканер', price: 2700, quantity: 3 },
+  { name: 'Дроид', price: 400, quantity: 7 },
+  { name: 'Захват', price: 1200, quantity: 9 },
+];
+
+function getAllPropValues(propName) {
+  // Пиши код ниже этой строки
+  const allPropValues = [];
+  for (const product of products) {
+    if (product[propName]) {
+      allPropValues.push(product[propName]);
+    }
+  }
+  
+  return allPropValues;
+  // Пиши код выше этой строки
+}
+
+
+// 20
+// Напиши функцию calculateTotalPrice(productName) которая принимает один параметр productName - название товара. 
+// Функция должна вернуть общую стоимость (цена * количество) товара с таким именем из массива products.
+
+const products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Сканер', price: 2700, quantity: 3 },
+  { name: 'Дроид', price: 400, quantity: 7 },
+  { name: 'Захват', price: 1200, quantity: 9 },
+];
+
+function calculateTotalPrice(productName) {
+  // Пиши код ниже этой строки
+
+  for (const product of products) {
+    if (product['name'] === productName) {
+      return product['price'] * product['quantity'];
+    }
+  }
+return 0;
+  // Пиши код выше этой строки
+}
+
+
+// 21
+// Пришел трёхдневный прогноз максимальных температур и мы считаем среднюю температуру за три дня (meanTemperature). 
+// Замени объявления переменных yesterday, today и tomorrow одной операцией деструктуризации свойств объекта highTemperatures.
+const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
+// Пиши код ниже этой строки
+const {yesterday, today, tomorrow} = highTemperatures;
+
+
+// Пиши код выше этой строки
+const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+
+// 22
+// В прогнозе максимальных температур также может быть необязательное свойство icon - иконка погоды. 
+// Замени объявления переменных yesterday, today, tomorrow и icon одной операцией деструктуризации свойств объекта highTemperatures. 
+// Задай значение по умолчанию для icon - строку 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg'.
+const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
+// Пиши код ниже этой строки
+const {
+  yesterday, 
+  today, 
+  tomorrow, 
+  icon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg',
+} = highTemperatures;
+
+
+// 23
+// Замени объявления переменных highYesterday, highToday, highTomorrow и highIcon одной операцией деструктуризации свойств объекта highTemperatures. 
+// Задай значение по умолчанию для highIcon - строку 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg'.
+const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
+// Пиши код ниже этой строки
+const {
+  yesterday: highYesterday,
+  today: highToday,
+  tomorrow: highTomorrow,
+  icon: highIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg',
+} = highTemperatures;
+
+// Пиши код выше этой строки
+const meanTemperature = (highYesterday + highToday + highTomorrow) / 3;
+
+
+
+//24
+// Выполни рефакторинг цикла for...of так, чтобы в нём использовалась деструктуризация объекта..
+
+const colors = [
+  { hex: '#f44336', rgb: '244,67,54' },
+  { hex: '#2196f3', rgb: '33,150,243' },
+  { hex: '#4caf50', rgb: '76,175,80' },
+  { hex: '#ffeb3b', rgb: '255,235,59' },
+];
+
+const hexColors = [];
+const rgbColors = [];
+// Пиши код ниже этой строки
+
+for (const {hex, rgb} of colors) {
+  hexColors.push(hex);
+  rgbColors.push(rgb);
+}
+
+
+// 25
+// Мы получили прогноз погоды на два дня, с минимальными и максимальными температурами, 
+//     а также необязательными иконками. Замени объявления всех переменных одной операцией деструктуризации 
+//     свойств объекта forecast. Задай значение по умолчанию для иконок, переменных todayIcon и tomorrowIcon - 
+//         строку 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg'.
+
+const forecast = {
+  today: {
+    low: 28,
+    high: 32,
+    icon: 'https://www.flaticon.com/svg/static/icons/svg/861/861059.svg',
+  },
+  tomorrow: {
+    low: 27,
+    high: 31,
+  },
+};
+// Пиши код ниже этой строки
+const {
+  today: {low: lowToday, 
+          high: highToday, 
+          icon: todayIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg'},
+  tomorrow: {low: lowTomorrow, 
+             high: highTomorrow,
+             icon: tomorrowIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg',
+            },
+} = forecast;
+  
+
+// 26
+// Функция calculateMeanTemperature(forecast) принимает один параметр forecast - объект температур на два дня следующего формата.
+// Пиши код ниже этой строки
+function calculateMeanTemperature(forecast) {
+  const {
+    today: {
+      low: todayLow,
+      high: todayHigh,
+    },
+    tomorrow: {
+      low: tomorrowLow,
+      high: tomorrowHigh,
+    }
+  } = forecast;
+
+
+  // Пиши код выше этой строки
+  return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
+}
+
+
+// 27
+// В переменной scores хранится массив результатов тестирования. Используя распыление и методы Math.max() и Math.min() дополни код так, 
+//     чтобы в переменной bestScore был самый высокий балл, а в worstScore самый низкий.
+
+
+
+
+// 28
+// В переменных firstGroupScores, secondGroupScores и thirdGroupScores хранятся результаты тестирования отдельных групп. Используя распыление дополни код так, чтобы:
+
+// В переменной allScores хранился массив всех результатов от первой до третьей группы.
+// В переменной bestScore был самый высокий общий балл.
+// В переменной worstScore был самый низкий общий балл.
+
+
+
+
+// 29
+// В конструкторе можно создавать новые тесты, для которых есть настройки по умолчанию которые хранятся в переменной defaultSettings. 
+// Во время создания теста, все или часть настроек можно переопределить, они хранятся в переменной overrideSettings.
+// Для того чтобы получить финальные настройки теста, необходимо взять настройки по умолчанию и поверх них применить переопределённые настройки. 
+// Дополни код так, чтобы в переменной finalSettings получился объект финальных настроек теста.
+
+
+
+// 30
+// Напиши функцию makeTask(data) которая принимает один параметр data - объект со следующими свойствами.
+// text - текст задачи.
+// category - категория задачи.
+// priority - приоритет задачи.
+// Функция должна составить и вернуть новый объект задачи, не изменяя напрямую параметр data. 
+// В новом объекте должно быть свойство completed, значение которого хранится в одноимённой локальной переменной.
+// В параметре data гарантированно будет только свойство text, а остальные два, category и priority, могут отсутствовать. 
+// Тогда, в новом объекте задачи, в свойствах category и priority должны быть значения по умолчанию, хранящиеся в одноимённых локальных переменных.
+
+function makeTask(data) {
+  const completed = false;
+  const category = 'Общее';
+  const priority = 'Обычный';
+  // Пиши код ниже этой строки
+
+  return {completed, category, priority, ...data};
+  // Пиши код выше этой строки
+}
+
+
+
+// 31
+// Используя операцию rest дополни код функции add() так, чтобы она принимала любое количество аргументов, считала и возвращала их сумму.
+// Пиши код ниже этой строки
+function add(...args) {
+  let sum = 0;
+  for (const arg of args) {
+    sum += arg;
+  }
+    return sum;
+  // Пиши код выше этой строки
+}
+
+
+// 32
+// Функция addOverNum() считает сумму всех аргументов. Измени параметры и тело функции addOverNum() так, 
+//     чтобы она считала сумму только тех аргументов, которые больше чем заданное число. Это число должно быть первым параметром функции.
+
+// Пиши код ниже этой строки
+function addOverNum(number, ...args) {
+  let total = 0;
+
+  for (const arg of args) {
+    if (arg > number) {
+    total += arg;
+    }
+  }
+
+  return total;
+  // Пиши код выше этой строки
+}
+
+// 33
+// Функция findMatches() принимает произвольное количество аргументов. Первым аргументом всегда будет массив чисел, а остальные аргументы будут просто числами.
+// Дополни код функции так, чтобы она возвращала новый массив matches, в котором будут только те аргументы, начиная со второго, которые есть в массиве первого аргумента.
+// Например, findMatches([1, 2, 3, 4, 5], 1, 8, 2, 7) должна вернуть массив [1, 2], потому что только они есть в массиве первого аргумента.
+// Пиши код ниже этой строки
+function findMatches(firstMatches, ...otherMatches) {
+  const matches = []; // Не изменяй эту строку
+for (const match of otherMatches) {
+  if (firstMatches.includes(match)) {
+      matches.push(match);
+}
+}
+  // Пиши код выше этой строки
+  return matches;
+}
+
+
+// 34
+// Добавь объекту bookShelf ещё два метода, которые пока что будут возвращать просто строки по аналогии с getBooks() и addBook(bookName).
+// Метод removeBook(bookName) будет удалять книгу по имени. Возвращает строку 'Удаляем книгу <имя книги>', где <имя книги> это значение параметра bookName.
+// Метод updateBook(oldName, newName) будет обновлять название книги на новое. Возвращает строку 'Обновляем книгу <старое имя> на <новое имя>', 
+//     где <старое имя> и <новое имя>это значения параметров oldName и newName соотвественно.
+const bookShelf = {
+  // Пиши код ниже этой строки
+  books: ['Последнее королевство', 'Страж снов'],
+  getBooks() {
+    return 'Возвращаем все книги';
+  },
+  addBook(bookName) {
+    return `Добавляем книгу ${bookName}`;
+  },
+  removeBook(bookName) {
+    return `Удаляем книгу ${bookName}`;
+  },
+  updateBook(oldName, newName) {
+    return `Обновляем книгу ${oldName} на ${newName}`;
+  },
+  // Пиши код выше этой строки
+};
+
+
+// 35
+// Дополни метод updateBook(oldName, newName) так, чтобы он изменял название книги с oldName на newName в свойстве books. 
+// Используй indexOf() для того, чтобы найти нужный элемент массива, и splice() для того чтобы заменить этот элемент.
+const bookShelf = {
+  books: ['Последнее королевство', 'Мгла', 'Страж снов'],
+  updateBook(oldName, newName) {
+    // Пиши код ниже этой строки
+	const bookIndex = this.books.indexOf(oldName);
+    this.books.splice(bookIndex, 1, newName);
+	
+    // Пиши код выше этой строки
+  },
+};
+
+// 36
+// К нам обратилась владелица лавки зелий «У старой жабы» и заказала программу для ведения инвентаря - добавления, удаления, поиска и обновления зелий. 
+// Добавь объекту atTheOldToad свойство potions, значением которого сделай пустой массив.
+const atTheOldToad = {
+  // Пиши код ниже этой строки
+  potions: [],
+  // Пиши код выше этой строки
+};
+
+
+// 37
+// Добавь объекту atTheOldToad метод getPotions(), который просто возвращает значение свойства potions.
+const atTheOldToad = {
+  // Пиши код ниже этой строки
+  potions: ['Зелье скорости', 'Дыхание дракона', 'Каменная кожа'],
+   getPotions() {
+    return this.potions;
+  },
+  // Пиши код выше этой строки
+};
+
+
+
+// 38
+// Дополни метод addPotion(potionName) так, чтобы он добавлял зелье potionName в конец массива зелий в свойстве potions.
+const atTheOldToad = {
+  potions: ['Зелье скорости', 'Дыхание дракона', 'Каменная кожа'],
+  addPotion(potionName) {
+    // Пиши код ниже этой строки
+        this.potions.push(potionName);
+    // Пиши код выше этой строки
+  },
+};
+
+
+
+// 39
+// Дополни метод removePotion(potionName) так, чтобы он удалял зелье potionName из массива зелий в свойстве potions.
+const atTheOldToad = {
+  potions: ["Зелье скорости", "Дыхание дракона", "Каменная кожа"],
+  removePotion(potionName) {
+    // Пиши код ниже этой строки
+const potionIndex = this.potions.indexOf(potionName);
+    this.potions.splice(potionIndex, 1);
+    
+    // Пиши код выше этой строки
+  },
+};
+
+
+// 40
+// Дополни метод updatePotionName(oldName, newName) так, чтобы он обновлял название зелья с oldName на newName, в массиве зелий в свойстве potions.
+const atTheOldToad = {
+  potions: ['Зелье скорости', 'Дыхание дракона', 'Каменная кожа'],
+  updatePotionName(oldName, newName) {
+    // Пиши код ниже этой строки
+ const indexPotion = this.potions.indexOf(oldName);
+    this.potions.splice(indexPotion, 1, newName);
+    // Пиши код выше этой строки
+  },
+};
+
+
+41
+
